4a11baa91ee96fceef2657af1c3940d79b7a5e99
Manu-Andaluz <manupeandaluz@gmail.com>
2022-08-04 16:35:48 -0300

    updating files

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://manu-andaluz.github.io/React-Menu-App/",
   plugins: [react()],
+  base: "/menu/index.html/",
 });
